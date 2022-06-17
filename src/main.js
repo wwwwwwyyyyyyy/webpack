96a61523023e4ad4f680f06f05b07fda94e9d019
lizhao <2986742990@qq.com>
2022-06-17 20:54:38 +0800

    打包css文件

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import {tab} from './tab.js'
 import {marquee} from './marquee.js'
+import './styles/index.css'
+// import '../public/index.html'
 tab()
 marquee()
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
-const path=require('path')
-module.exports={
-    entry:"./src/main.js",
-    output:{
-        path:path.join(__dirname, 'lib'),
-        filename:"index.js",
-        clean:true
-    }
-}
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "production",
+  entry: "./src/main.js",
+  output: {
+    path: path.join(__dirname, "lib"),
+    filename: "index.js",
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "./public/index.html"),
+    }),
+  ],
+  devServer: {
+    open: true,
+    port: 30000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+
+};
