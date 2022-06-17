476d4b868f7f6b2870d1ebb2e414247b8252d054
lizhao <2986742990@qq.com>
2022-06-17 21:07:04 +0800

    完成了less打包

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import {tab} from './tab.js'
 import {marquee} from './marquee.js'
 import './styles/index.css'
+import './styles/marquee.less'
 // import '../public/index.html'
 tab()
 marquee()
--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,10 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
     ],
   },
-
 };
